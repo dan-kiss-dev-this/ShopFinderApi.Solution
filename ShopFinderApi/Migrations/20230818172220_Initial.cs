5eb9a399ec7d3cbe35ef291a2a1b29b9dccc8b10
Daniel Kiss <flowfast47@gmail.com>
2023-08-18 10:24:05 -0700

    add initial migration

---
+++ ShopFinderApi/Migrations/20230818172220_Initial.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ShopFinderApi.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Shops",
+                columns: table => new
+                {
+                    ShopId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Description = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Rating = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Shops", x => x.ShopId);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.InsertData(
+                table: "Shops",
+                columns: new[] { "ShopId", "Description", "Rating" },
+                values: new object[] { 1, "Larry's Rock Climbing", 5 });
+
+            migrationBuilder.InsertData(
+                table: "Shops",
+                columns: new[] { "ShopId", "Description", "Rating" },
+                values: new object[] { 2, "DogLand", 5 });
+
+            migrationBuilder.InsertData(
+                table: "Shops",
+                columns: new[] { "ShopId", "Description", "Rating" },
+                values: new object[] { 3, "Paint-topia", 4 });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Shops");
+        }
+    }
+}
