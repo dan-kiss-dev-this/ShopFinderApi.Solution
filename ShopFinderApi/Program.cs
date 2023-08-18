345c5c290c1f9e5c0303576259f36790b39d7bec
Daniel Kiss <flowfast47@gmail.com>
2023-08-18 10:13:29 -0700

    add service to include AddDbContext

--- ShopFinderApi/Program.cs
+++ ShopFinderApi/Program.cs
@@ -1,8 +1,21 @@
+using ShopFinderApi.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<ShopFinderApiContext>(
+    dbContextOptions => dbContextOptions
+      .UseMySql(
+        builder.Configuration["ConnectionStrings:DefaultConnection"],
+        ServerVersion.AutoDetect(
+            builder.Configuration["ConnectionStrings:DefaultConnection"]
+        )
+      )
+);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -19,7 +32,6 @@ else
 {
     app.UseHttpsRedirection();
 }
-
 
 app.UseAuthorization();
 
