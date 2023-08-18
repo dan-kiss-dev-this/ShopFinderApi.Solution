3da2e1c99006555e6bc7407cd10330ce00fc0d9c
Daniel Kiss <flowfast47@gmail.com>
2023-08-18 15:57:19 -0700

    add user

---
+++ ShopFinderApi/Models/User.cs
@@ -0,0 +1,9 @@
+namespace ShopFinderApi.Models
+{
+  public class User
+  {
+    public int UserId { get; set; }
+    public string UserName { get; set; }
+    public string Password { get; set; }
+  }
+}
