beb62771195da7b8c03367ce3e3be10daa76d213
Daniel Kiss <flowfast47@gmail.com>
2023-08-18 10:07:32 -0700

    add context with seed data

---
+++ ShopFinderApi/Models/ShopFinderApiContext.cs
@@ -0,0 +1,21 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace ShopFinderApi.Models;
+
+public class ShopFinderApiContext : DbContext
+{
+  public DbSet<Shop> Shops { get; set; }
+
+  public ShopFinderApiContext(DbContextOptions<ShopFinderApiContext> options) : base(options) { }
+
+  //include seed data
+  protected override void OnModelCreating(ModelBuilder builder)
+  {
+    builder.Entity<Shop>()
+      .HasData(
+        new Shop { ShopId = 1, Description = "Larry's Rock Climbing", Rating = 5 },
+        new Shop { ShopId = 2, Description = "DogLand", Rating = 5 },
+        new Shop { ShopId = 2, Description = "Paint-topia", Rating = 4 }
+      );
+  }
+}
